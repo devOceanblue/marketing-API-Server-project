eff6d566cd635a5c41e04e5e6263c4f6525b5715
kimseungyoon <neptuner24@loplat.com>
2022-12-02 18:51:27 +0900

    테스트커버리지 보충

--- tests/unit/routes/test_reward.py
+++ tests/unit/routes/test_reward.py
@@ -1,6 +1,10 @@
 import random
 
 import pytest
+from sqlalchemy import delete
+
+from app.models.orm_models.reward import Reward
+from db_setup import SessionLocal
 
 
 class TestEarnReward:
@@ -9,6 +13,11 @@ class TestEarnReward:
         data = {"ad_campaign_id": 2}
         resp = client.patch(f"/reward/earn/1", json=data)
         assert resp.status_code == 200
+
+        session = SessionLocal()
+        stmt = delete(Reward).where(Reward.user_id == 1)
+        session.execute(stmt)
+        session.commit()
 
     @pytest.mark.asyncio
     async def test_earn_reward_fail_if_already_earned(self, client):
@@ -20,13 +29,27 @@ class TestEarnReward:
         resp = client.patch(f"/reward/earn/1", json=data)
         assert resp.status_code == 404
 
+        session = SessionLocal()
+        stmt = delete(Reward).where(Reward.user_id == 1)
+        session.execute(stmt)
+        session.commit()
+
 
 class TestGetWeeklyRewards:
     @pytest.mark.asyncio
     async def test_get_weekly_rewards(self, client):
+        data = {"ad_campaign_id": 1}
+        resp = client.patch(f"/reward/earn/4", json=data)
+        assert resp.status_code == 200
+
         client.get("/advertisement?user_id=4&gender=M&country=KR")
         resp = client.get("/reward")
         assert resp.status_code == 200
+
+        session = SessionLocal()
+        stmt = delete(Reward).where(Reward.user_id == 4)
+        session.execute(stmt)
+        session.commit()
 
 
 class TestGetBalanceOfRewards:
